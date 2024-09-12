f5907345a67d4e0061b185e6314558d8ed42aaac
mohamed gamal <121718578+Gemy33@users.noreply.github.com>
2024-09-12 16:10:26 +0300

    Update wishlist.service.ts

--- src/app/core/services/wishlist.service.ts
+++ src/app/core/services/wishlist.service.ts
@@ -9,46 +9,43 @@ import { isPlatformBrowser } from '@angular/common';
   providedIn: 'root',
 })
 export class WishlistService {
+     token!:any;
+
   // https://ecommerce.routemisr.com/api/v1/wishlist
   _PLATFORM_ID = inject(PLATFORM_ID);
 
-  constructor(private _HttpClient: HttpClient) {}
-  add_wishlist(_id: string): Observable<any> {
-    let token='';
-    if (isPlatformBrowser(this._PLATFORM_ID)) {
-      token =( localStorage.getItem('userData')!);
+  constructor(private _HttpClient: HttpClient) {
+     if (isPlatformBrowser(this._PLATFORM_ID)) {
+      this.token={token:localStorage.getItem('userData')!}
     }
+  }
+  add_wishlist(_id: string): Observable<any> {
+  
     return this._HttpClient.post(
       `${envionment.baseUrl}/api/v1/wishlist`,
       {
         productId: _id,
       },
-      { headers: { 'token': token} }
+      { headers: this.token }
     );
   }
   remove_item_from_wishlist(id: string): Observable<any> {
-    let token='';
-    if (isPlatformBrowser(this._PLATFORM_ID)) {
-      token =( localStorage.getItem('userData')!);
-    }
+   
     return this._HttpClient.delete(
       `${envionment.baseUrl}/api/v1/wishlist/${id}`,
       {
-        headers: {
-          token: token
-        },
+        headers: 
+          this.token
+       
       }
     );
   }
-  get_logged_user_wishlist() {
-    let token='';
-    if (isPlatformBrowser(this._PLATFORM_ID)) {
-      token =( localStorage.getItem('userData')!);
-    }
+  get_logged_user_wishlist(): Observable<any> {
+  
     return this._HttpClient.get(`${envionment.baseUrl}/api/v1/wishlist`, {
-      headers: {
-        token: token 
-      },
+      headers: 
+         this.token 
+      
     });
   }
 }
